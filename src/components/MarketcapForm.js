ccbe3985273cfab0df3736fd5f555e0eda8a3542
Imagineer-Ty Github <tyrone.brown5311@gmail.com>
2023-01-20 21:27:12 -0500

    init

---
+++ src/components/MarketcapForm.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+function MarketcapForm() {
+    const [input, setInput] = useState('');
+
+  return (
+    <form className='marketcap-form'>
+        <input
+        type='number'
+        placeholder='Enter Current Price'
+        value={input}
+        name='current-price'
+        className='current-price-input'
+        />
+        <input
+        type='number'
+        placeholder='Enter Circulating Supply'
+        value={input}
+        name='circulating-supply'
+        className='circulating-supply-input'
+        />
+        <button  className='enter-button'>
+            Calculate
+        </button>
+
+
+    </form>
+  )
+}
+
+export default MarketcapForm
+
+
+
+
+
+
+
+
+
